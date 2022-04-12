fbd5c3abbc95ae4ca7ed7392d73c737798f9f1e2
RIDVAN <ridvancakirtr@gmail.com>
2022-04-12 11:20:25 +0300

    axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/api/v1/'
+const baseURL = 'https://compilertestapi.herokuapp.com/api/v1/'
 export default axios.create({
   baseURL,
   headers: {
